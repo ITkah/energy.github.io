634f103ece040de28b067d7645bff7f66ef8007e
ITkah <ilatkacenko132@gmail.com>
2020-04-01 21:51:10 +0300

    end

--- js/common.js
+++ js/common.js
@@ -2,4 +2,25 @@ $(document).ready(function() {
     $(".menu-btn").on("click", function(){
         $("nav").slideToggle(200);
     });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('nav').addClass("fixed-header");
+        } else {
+            $('nav').removeClass("fixed-header");
+        }
+    });
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+    $('.single').slick({
+        infinite: true,
+        dots: true,
+        arrows: false,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
 });
